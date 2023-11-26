a62dde30a62a1c51921d6c679fe113404bc016ff
TeeTys <133400656+TeeTys@users.noreply.github.com>
2023-11-26 12:39:40 +0000

    Update teste.c

--- teste.c
+++ teste.c
@@ -22,14 +22,14 @@ int main(int argc, char *argv[]) {
     int fd;
     char buf[512];
 
-    // Verificar se a quantidade de argumentos está correta
+    // Verify Arguments
     
-    if (argc < 2 || (argc % 2) != 1){ //|| strcmp(argv[1], "--P") != 0 || strcmp(argv[3], "--I") != 0 || strcmp(argv[5],"--A") != 0) {
+    if (argc < 2 || (argc % 2) != 1){ 
         print_usage(argv[0]);
         exit(EXIT_FAILURE);
     } 
 
-    // Abrir o arquivo do módulo do kernel
+    // Open kernel module archive
     fd = open(DEVICE_PATH, O_WRONLY);
     if (fd == -1) {
         perror("Falha ao abrir o arquivo do módulo do kernel");
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
     }
     
 
-    // Construir a mensagem a ser enviada para o módulo do kernel
+    // Build the message to the kernel
     struct UserArguments args;
     snprintf(args.arg1, sizeof(args.arg1), "NULL");
     snprintf(args.arg2, sizeof(args.arg2), "NULL");
@@ -54,26 +54,9 @@ int main(int argc, char *argv[]) {
 	    	snprintf(args.arg3, sizeof(args.arg3), argv[i+1]);
 	} 
     }
-//  if (strcmp(argv[6],"ALLOW") != 0 && strcmp(argv[6],"BLOCK") != 0) {
-//    if (strcmp(args.arg1,"") == 0) {
-//	snprintf(args.arg1, sizeof(args.arg3), "NULL");
-//    }	
 
-//    if (strcmp(args.arg2,"") == 0) {
-//	snprintf(args.arg2, sizeof(args.arg3), "NULL");
-//    }
-
-//    if (strcmp(args.arg3,"") == 0) {
-//	snprintf(args.arg3, sizeof(args.arg3), "NULL");
-//    }
-
-    //buf[0] = '\0';
-    //for (int i = 1; i < argc; i += 2){
-    //	snprintf(buf + strlen(buf), sizeof(buf)-strlen(buf), "%s ", argv[i+1]);
-    //}
-    // Escrever a mensagem no arquivo do módulo do kernel
-    //printf("%s \n", buf);
-    //trimTrailing(buf);
+    // Write the message in kernel module archive
+   
     if (write(fd, &args, sizeof(args)) == -1) {
         perror("Falha ao escrever no arquivo do módulo do kernel");
         close(fd);
@@ -82,31 +65,8 @@ int main(int argc, char *argv[]) {
 
     printf("Mensagem enviada para o módulo do kernel.\n");
 
-    // Fechar o arquivo do módulo do kernel
+    // Close kernel module archive
     close(fd);
 
     return 0;
-}
-
-void trimTrailing(char * str)
-{
-    int index, i;
-
-    /* Set default index */
-    index = -1;
-
-    /* Find last index of non-white space character */
-    i = 0;
-    while(str[i] != '\0')
-    {
-        if(str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-        {
-            index= i;
-        }
-
-        i++;
-    }
-
-    /* Mark next character to last non-white space character as NULL */
-    str[index + 1] = '\0';
 }
